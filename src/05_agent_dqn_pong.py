ab2b7c630d3e112309cc906fa9f1f71186ab5987
michal <michal.nand@gmail.com>
2019-02-06 11:25:12 +0100

    common

--- src/05_agent_dqn_pong.py
+++ src/05_agent_dqn_pong.py
@@ -15,7 +15,7 @@ env.print_info()
 
 #init DQN agent
 #you can choose from pre-saved neteworks a, b, c
-agent = libs_agent.agent_dqn.DQNAgent(env, "networks/pong_network_c/parameters.json", 0.2, 0.01, 0.99999)
+agent = libs_agent.agent_dqn.DQNAgent(env, "networks/pong_network_b/parameters.json", 0.2, 0.01, 0.99999)
 
 
 #process training
@@ -27,9 +27,9 @@ for iteration in range(0, training_iterations):
     if iteration%100 == 0:
         print(iteration*100.0/training_iterations, env.get_score())
 
-agent.save("networks/pong_network_c/trained/")
+agent.save("networks/pong_network_b/trained/")
 
-agent.load("networks/pong_network_c/trained/")
+agent.load("networks/pong_network_b/trained/")
 
 
 #reset score
