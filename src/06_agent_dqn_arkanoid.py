bf8ac7c3c33eea628f43b9f80016626af0c60575
michalnand <michal.nand@gmail.com>
2019-02-15 23:17:31 +0100

    arkanoid net

--- src/06_agent_dqn_arkanoid.py
+++ src/06_agent_dqn_arkanoid.py
@@ -9,7 +9,7 @@ env = libs_env.env_arkanoid.EnvArkanoid()
 env.print_info()
 
 '''
-agent = lib.agent.Agent(env)
+agent = libs_agent.agent.Agent(env)
 while True:
     agent.main()
 
@@ -17,17 +17,25 @@ while True:
     env.render()
 '''
 
-#init DQN agent
-agent = lib.agent_dqn.DQNAgent(env, "networks/arkanoid_network_parameters.json")
 
-#simulate training
-training_iterations = 100000
+#init DQN agent
+agent = libs_agent.agent_dqn.DQNAgent(env, "networks/arkanoid_network_a/parameters.json", 0.2, 0.02, 0.999999)
+
+
+
+#process training
+training_iterations = 200000
 
 for iteration in range(0, training_iterations):
     agent.main()
     #print training progress %, ane score, every 100th iterations
     if iteration%100 == 0:
         print(iteration*100.0/training_iterations, env.get_score())
+
+agent.save("networks/arkanoid_network_a/trained/")
+
+agent.load("networks/arkanoid_network_a/trained/")
+
 
 #reset score
 env.reset_score()
@@ -37,12 +45,14 @@ agent.run_best_enable()
 
 
 #process testing iterations
-testing_iterations = 2000
-#for iteration in range(0, testing_iterations):
+testing_iterations = 10000
+for iteration in range(0, testing_iterations):
+    agent.main()
+    print("move=", env.get_move(), " score=",env.get_score())
+
+
 while True:
     agent.main()
-
-    print("move=", env.get_move(), " score=",env.get_score())
     env.render()
 
 print("program done")
--- src/libs_env/env_arkanoid.py
+++ src/libs_env/env_arkanoid.py
@@ -1,5 +1,6 @@
 import libs_env.env
-import gl_gui.gl_gui as gl_gui
+import libs_gl_gui.gl_gui as gl_gui
+
 
 import numpy
 import time
@@ -14,35 +15,30 @@ class EnvArkanoid(libs_env.env.Env):
         #init parent class -> environment interface
         libs_env.env.Env.__init__(self)
 
-        width  = 8
-        height = 20
-
-        #dimensions 1x10x10
-        self.width  = 2*width
-        self.height = height
+        #dimensions
+        self.width  = 16
+        self.height = 20
         self.depth  = 3
 
         #init state, as 1D vector (tensor with size depth*height*width)
         self.observation    = numpy.zeros(self.get_size())
 
-        #4 actions for movements
+        #3 actions for movements
         self.actions_count  = 3
 
+        #player paddle size
         self.player_size = 3
 
+        #init game
         self.reset()
-        self.__position_to_state()
+        self.__update_state()
 
         self.gui = gl_gui.GLVisualisation()
-
         self.size_ratio = self.width/self.height
 
 
 
     def reset(self):
-        #initial player position
-        self.player = self.width/2
-
         self.board    = numpy.zeros((self.height, self.width))
 
         for x in range(0, self.width):
@@ -53,27 +49,113 @@ class EnvArkanoid(libs_env.env.Env):
             self.board[8][x] = 5
             self.board[9][x] = 6
 
-        self.__reset_ball()
+        self.__respawn()
 
-    def __reset_ball(self):
+    def __respawn(self):
+        #init player position
+        self.player_position = self.width/2
+
+        #init ball position
         self.ball_x = int(self.width/2) + random.randint(-1, 1)
-        self.ball_y = int(self.height/2)+ random.randint(-1, 1)
+        self.ball_y = int(self.height/2 + 1) + random.randint(-1, 1)
 
-        if random.randint(0, 1) == 0:
-            self.ball_dx = 1
-        else:
-            self.ball_dx = -1
-
-        if random.randint(0, 1) == 0:
-            self.ball_dy = 1
-        else:
-            self.ball_dy = -1
-
+        self.ball_dx = 1
+        self.ball_dy = -1
 
 
     def _print(self):
         print("move=", self.get_move(), "  score=", self.get_score(), "  normalised score=", self.get_normalised_score())
         self.render()
+
+    def do_action(self, action):
+
+        if action == 0:
+            self.player_position+= 1
+        if action == 1:
+            self.player_position-= 1
+
+        self.player_position = self.__saturate(self.player_position, 1, self.width-2)
+
+        result = self.__process_ball()
+
+
+        self.reward = 0.0
+        self.set_no_terminal_state()
+
+
+        if result == "brick":
+            self.reward = 0.1
+
+
+        if result == "miss":
+            self.reward = -1.0
+            self.set_terminal_state()
+            self.__respawn()
+
+
+        if self.__count_remaining() <= 2*3:
+            self.reward = 1.0
+            self.set_terminal_state()
+            self.reset()
+
+
+
+        self.__update_state()
+        self.next_move()
+
+    def __update_state(self):
+        self.observation.fill(0.0)
+
+        '''
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                if self.board[y][x]!= 0:
+                    self.__observation_set_dirrect(x, y, 0, 1.0)
+        '''
+
+        ball_x = self.__saturate(int(self.ball_x), 0, self.width-1)
+        ball_y = self.__saturate(int(self.ball_y), 0, self.height-1)
+
+        player_x = self.__saturate(int(self.player_position), 0, self.width-1)
+        player_y = self.height-1
+
+
+        self.__observation_set_dirrect(ball_x, ball_y, 1, 1.0)
+        self.__observation_set_dirrect(player_x, player_y, 2, 1.0)
+
+        for i in range(0, len(self.observation)):
+            self.observation[i]+= random.random()*0.01
+
+
+        '''
+        ball_x = self.__saturate(int(self.ball_x), 0, self.width-1)
+        ball_y = self.__saturate(int(self.ball_y), 0, self.height-1)
+
+        player_x = self.__saturate(int(self.player_position), 0, self.width-1)
+        player_y = self.height-1
+
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                if self.board[y][x]!= 0:
+                    color = self.__item_to_color(self.board[y][x])
+                    self.__observation_set(x, y, color)
+
+        color = self.__item_to_color(10)
+        self.__observation_set(ball_x, ball_y, color)
+
+        color = self.__item_to_color(11)
+        self.__observation_set(player_x, player_y, color)
+        '''
+
+
+    def __observation_set(self, x, y, color):
+        self.observation[(0*self.height + y)*self.get_width() + x] = color[0]
+        self.observation[(1*self.height + y)*self.get_width() + x] = color[1]
+        self.observation[(2*self.height + y)*self.get_width() + x] = color[2]
+
+    def __observation_set_dirrect(self, x, y, z, value):
+        self.observation[(z*self.height + y)*self.get_width() + x] = value
+
 
     def render(self):
         self.gui.init("arkanoid", 32*self.width, 32*self.height)
@@ -81,9 +163,9 @@ class EnvArkanoid(libs_env.env.Env):
         self.gui.start()
 
         if self.height > self.width:
-            element_size = 1.9/self.height
+            element_size = 1.99/self.height
         else:
-            element_size = 1.9/self.width
+            element_size = 1.99/self.width
 
         for y in range(0, self.height):
             for x in range(0, self.width):
@@ -93,14 +175,14 @@ class EnvArkanoid(libs_env.env.Env):
 
                     self.gui.set_color(color[0], color[1], color[2])
 
-                    self.gui.translate(self.x_to_gui_x(x), self.y_to_gui_y(y), 0.0)
+                    self.gui.translate(self.__x_to_gui_x(x), self.__y_to_gui_y(y), 0.0)
                     self.gui.paint_square(element_size)
                     self.gui.pop()
 
         self.gui.push()
         color = self.__item_to_color(10)
         self.gui.set_color(color[0], color[1], color[2])
-        self.gui.translate(self.x_to_gui_x(self.ball_x), self.y_to_gui_y(self.ball_y), 0.0)
+        self.gui.translate(self.__x_to_gui_x(self.ball_x), self.__y_to_gui_y(self.ball_y), 0.0)
         self.gui.paint_square(element_size)
         self.gui.pop()
 
@@ -108,100 +190,60 @@ class EnvArkanoid(libs_env.env.Env):
             self.gui.push()
             color = self.__item_to_color(11)
             self.gui.set_color(color[0], color[1], color[2])
-            self.gui.translate(self.x_to_gui_x(self.player + i - self.player_size//2), self.y_to_gui_y(self.height-1), 0.0)
+            self.gui.translate(self.__x_to_gui_x(self.player_position + i - self.player_size//2), self.__y_to_gui_y(self.height-1), 0.0)
             self.gui.paint_square(element_size)
             self.gui.pop()
 
         self.gui.finish()
-        time.sleep(0.001)
+        time.sleep(0.05)
 
-    def do_action(self, action):
+    def __process_ball(self):
 
-        self.ball_x+= self.ball_dx
-        self.ball_y+= self.ball_dy
+        result = "none"
 
-        self.ball_x = self.__saturate(self.ball_x, 0, self.get_width()-1)
-        self.ball_y = self.__saturate(self.ball_y, 0, self.get_height()-1)
-
-        colission = False
         if self.board[self.ball_y][self.ball_x] != 0:
-            self.board[self.ball_y][self.ball_x] = 0
-            if self.ball_x+1 < self.get_width():
-                if self.board[self.ball_y][self.ball_x+1] != 0:
-                    self.board[self.ball_y][self.ball_x+1] = 0
-            colission = True
+            x1 = 2*(self.ball_x//2) + 0
+            x2 = 2*(self.ball_x//2) + 1
 
+            x1 = self.__saturate(x1, 0, self.width-1)
+            x2 = self.__saturate(x2, 0, self.width-1)
 
-        if colission:
-            self.ball_dy*= -1
+            self.board[self.ball_y][x1] = 0
+            self.board[self.ball_y][x2] = 0
 
-        if action == 0:
-            self.player+= 0
-        elif action == 1:
-            self.player+= 1
-        else:
-            self.player-= 1
-
-        self.player = self.__saturate(self.player, 0, self.width)
-
-        self.reward = 0.0
-
-        if colission:
-            self.reward = 0.25
+            self.ball_dy = 1
+            result = "brick"
 
         if self.ball_x <= 0:
             self.ball_dx = 1
 
-
-        if self.ball_x >=  self.width-1:
+        if self.ball_x >= self.width-1:
             self.ball_dx = -1
-
-        if self.ball_y >= self.height-1:
-            if self.player-self.ball_x == 0:
-                self.ball_dy = -1
-            elif self.player-self.ball_x == 1:
-                self.ball_dy = -1
-                self.ball_dx*= -1
-            elif self.player-self.ball_x == -1:
-                self.ball_dy = -1
-                self.ball_dx*= -1
-            else:
-                self.__reset_ball()
-                self.reward = -1.0
 
         if self.ball_y <= 0:
             self.ball_dy = 1
 
-        self.set_no_terminal_state()
-        if self.__count_remaining() < 10:
-            self.reward = 1.0
-            self.reset()
-            self.set_terminal_state()
+        if self.ball_y >= self.height-2:
+            if self.player_position == self.ball_x or self.player_position-1 == self.ball_x or self.player_position+1 == self.ball_x:
+                self.ball_dy = -1
+                result = "hit"
+            else:
+                result = "miss"
 
-        self.__position_to_state()
 
-        self.next_move()
+        self.ball_x+= self.ball_dx
+        self.ball_y+= self.ball_dy
 
-    def x_to_gui_x(self, x):
+        return result
+
+    def __x_to_gui_x(self, x):
         return self.size_ratio*(x*1.0/self.width - 0.5)*2.0
 
-    def y_to_gui_y(self, y):
+    def __y_to_gui_y(self, y):
         return -(y*1.0/self.height - 0.5)*2.0
 
-    def __position_to_state(self):
-        ball_x = self.__saturate(int(self.ball_x), 0, self.width-1)
-        ball_y = self.__saturate(int(self.ball_y), 0, self.height-1)
-
-        player_0 = self.__saturate(int(self.player_0), 0, self.height-1)
-        player_1 = self.__saturate(int(self.player_1), 0, self.height-1)
-
-        self.observation.fill(0.0)
-        self.observation[ball_y*self.get_width() + ball_x] = 1.0
-        self.observation[player_0*self.get_width() + 0] = 1.0
-        self.observation[player_1*self.get_width() + self.get_width()-1] = 1.0
-
     def __item_to_color(self, item_idx):
-        result = [0.2, 0.2, 0.2]
+        result = [0.0, 0.0, 0.0]
 
         if item_idx == 1:
             result = [1.0, 0.0, 0.0]
@@ -216,7 +258,7 @@ class EnvArkanoid(libs_env.env.Env):
         elif item_idx == 6:
             result = [0.0, 0.0, 1.0]
         elif item_idx == 10:
-            result = [1.0, 1.0, 1.0]
+            result = [1.0, 0.0, 1.0]
         elif item_idx == 11:
             result = [1.0, 1.0, 1.0]
         return result
@@ -229,34 +271,6 @@ class EnvArkanoid(libs_env.env.Env):
             value = min
 
         return value
-
-    def __position_to_state(self):
-        ball_x = self.__saturate(int(self.ball_x), 0, self.width-1)
-        ball_y = self.__saturate(int(self.ball_y), 0, self.height-1)
-
-        player_x = self.__saturate(int(self.player), 0, self.width-1)
-        player_y = self.height-1
-
-        self.observation.fill(0.0)
-
-        for y in range(0, self.height):
-            for x in range(0, self.width):
-                if self.board[y][x]!= 0:
-                    color = self.__item_to_color(self.board[y][x])
-
-                    self.observation[(0*self.height + y)*self.get_width() + x] = color[0]
-                    self.observation[(1*self.height + y)*self.get_width() + x] = color[1]
-                    self.observation[(2*self.height + y)*self.get_width() + x] = color[2]
-
-        color = self.__item_to_color(10)
-        self.observation[(0*self.height + ball_y)*self.get_width() + ball_x] = color[0]
-        self.observation[(1*self.height + ball_y)*self.get_width() + ball_x] = color[1]
-        self.observation[(2*self.height + ball_y)*self.get_width() + ball_x] = color[2]
-
-        color = self.__item_to_color(11)
-        self.observation[(0*self.height + player_y)*self.get_width() + player_x] = color[0]
-        self.observation[(1*self.height + player_y)*self.get_width() + player_x] = color[1]
-        self.observation[(2*self.height + player_y)*self.get_width() + player_x] = color[2]
 
     def __count_remaining(self):
         count = 0
